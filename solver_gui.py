19da8f871ad01e0995174280177d2b4ec7d065c3
unknown <halitanil.donmez@gmail.com>
2020-01-01 01:24:43 +0100

    adding repaint events

--- solver_gui.py
+++ solver_gui.py
@@ -3,11 +3,12 @@ from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLCDNumber, QDialog, QPushButton, QVBoxLayout, QGroupBox, \
     QGridLayout, QHBoxLayout, QLabel, QFrame
 import sys
-from PyQt5.QtGui import QPainter, QBrush, QPen, QLinearGradient, QColor
+from PyQt5.QtGui import QPainter, QBrush, QPen, QLinearGradient, QColor, QPaintEvent
 from PyQt5.QtCore import Qt, QRectF
 
 
 class App(QDialog):
+    labels = []
     def __init__(self):
         super().__init__()
         self.horizontalGroupBox = QGroupBox("")
@@ -27,28 +28,30 @@ class App(QDialog):
         self.setLayout(windowLayout)
         self.show()
 
+    def paintEvent(self, *args, **kwargs):
+        self.labels[1].setStyleSheet("background-color: black;")
+
     def create_grid_layout(self):
         layout = QGridLayout()
         layout.setColumnStretch(1, 4)
         layout.setColumnStretch(2, 4)
         layout.setSpacing(0)
-
         counter = 0
         for i in range(9):
             for j in range(9):
                 style_val = "background-color: white;"
-                b = QLabel()
-                b.setAlignment(Qt.AlignCenter)
-                b.setText(str(counter))
-                b.setFixedSize(80, 60)
+                label = QLabel()
+                label.setAlignment(Qt.AlignCenter)
+                label.setText(str(counter))
+                label.setFixedSize(80, 60)
                 if j % 3 == 0 and j != 0:
                     style_val += "border-left: 3px solid black;"
                 if i == 3 or i == 6:
                     style_val += "border-top: 3px solid black;"
-                b.setStyleSheet(style_val)
-                layout.addWidget(b, i, j)
+                label.setStyleSheet(style_val)
+                self.labels.append(label)
+                layout.addWidget(label, i, j)
                 counter += 1
-
         self.horizontalGroupBox.setLayout(layout)
 
 
