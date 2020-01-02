5562f91d9bc99cb1424854a7ed2689ca3e81b92f
unknown <halitanil.donmez@gmail.com>
2020-01-02 21:59:08 +0100

    using different timer

--- solver_gui.py
+++ solver_gui.py
@@ -1,7 +1,9 @@
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QGroupBox, \
     QGridLayout, QLabel
 import sys
-from PyQt5.QtCore import Qt
+import time
+from PyQt5.QtCore import Qt, QTime, QTimer
 
 
 class SudokuLabel(QLabel):
@@ -45,6 +47,7 @@ class App(QDialog):
         [1, 2, 0, 0, 0, 7, 4, 0, 0],
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
     ]
+    tried_locs = []
 
     def __init__(self):
         super().__init__()
@@ -54,7 +57,7 @@ class App(QDialog):
         self.top = 10
         self.width = 320
         self.height = 100
-        self.startTimer(5000)
+        QTimer.singleShot(5000, self.solveBoard)
 
     def initUI(self):
         self.setWindowTitle(self.title)
@@ -68,21 +71,34 @@ class App(QDialog):
     def showEvent(self, event):
         self.initUI()
 
-    def timerEvent(self, *args, **kwargs):
-        e_r, e_c = find_empty(self.board)
-        print("timer? ", e_r, " ", e_c)
-        if e_r == -1 and e_c == -1:
+    def solveBoard(self):
+        if len(self.labels) == 0:
             return
+        e_r, e_c = find_empty(self.board)
+        if e_r == -1 and e_c == -1:
+            return True
         empty_row = self.board[e_r]
         empty_col = [sub[e_c] for sub in self.board]
-        rr = int(e_r / 3) * 3
-        cc = int(e_c / 3)
-        g_c = [sub[cc * 3:cc * 3 + 3] for sub in self.board[rr:rr + 3]]
-        for r_c in range(1, 10):
-            if r_c not in empty_row and r_c not in empty_col and is_in_grid(g_c, r_c) is False:
-                self.labels[9*e_r + e_c].setText(str(r_c))
-                self.labels[9*e_r + e_c].setStyleSheet("background-color: red;")
-                self.board[e_r][e_c] = r_c
+
+        grid_row_index = int(e_r / 3) * 3
+        grid_col_index = int(e_c / 3)
+
+        g_c = [sub[grid_col_index * 3:grid_col_index * 3 + 3] for sub in self.board[grid_row_index:grid_row_index + 3]]
+        for cur_row_index in range(1, 10):
+            time.sleep(1)
+            self.labels[9 * e_r + e_c].setText(str(cur_row_index))
+            self.labels[9 * e_r + e_c].setStyleSheet("background-color: red;")
+            self.repaint()
+            if cur_row_index not in empty_row and \
+                    cur_row_index not in empty_col and \
+                    is_in_grid(g_c, cur_row_index) is False:
+                self.board[e_r][e_c] = cur_row_index
+                if self.solveBoard():
+                    return True
+        self.board[e_r][e_c] = 0
+        self.labels[9 * e_r + e_c].setText("")
+        self.labels[9 * e_r + e_c].setStyleSheet("background-color: white;")
+        return False
 
     def create_grid_layout(self):
         layout = QGridLayout()
@@ -93,11 +109,17 @@ class App(QDialog):
             for j in range(9):
                 style_val = "background-color: white;"
                 label = SudokuLabel()
+                font = QFont()
+                font.setBold(True)
+                font.setPointSize(20)
                 label.setAlignment(Qt.AlignCenter)
+                label.setFont(font)
                 cur = self.board[i][j]
                 if cur != 0:
                     label.setText(str(cur))
                     label.set_value(cur)
+                else:
+                    label.set_value(0)
                 label.setFixedSize(80, 60)
                 if j % 3 == 0 and j != 0:
                     style_val += "border-left: 3px solid black;"
@@ -108,7 +130,12 @@ class App(QDialog):
                 layout.addWidget(label, i, j)
         self.horizontalGroupBox.setLayout(layout)
 
+
 if __name__ == '__main__':
+    li = [1, 2, 4]
+    b = li[1]
+    b = 55
+    print(li)
     app = QApplication(sys.argv)
     ex = App()
     ex.show()
